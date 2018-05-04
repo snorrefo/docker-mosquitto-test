b4c1c46ff90482062eb1197dbfca2fc59afa8a9a
Snorre Foss Westman <snorrefo@gmail.com>
2018-05-04 22:08:39 +0200

    paho: cleaned up Dockerfile

--- test-paho/src/app.py
+++ test-paho/src/app.py
@@ -54,16 +54,16 @@ client.connect("mqtt", 1883, 60)
 client.loop_start()
 
 
-class MQTT_raw(object):
+class MQTT_ch_set(object):
 
     def __init__(self):
-        self.hostname = str
-        self.rssi = int
+        self.channel = str
+        self.newvalue = float
 
 
-mqtt_raw_01 = MQTT_raw()
-mqtt_raw_01.hostname = "esp"
-mqtt_raw_01.rssi = -83
+mqtt_ch0_set = MQTT_ch_set()
+mqtt_ch0_set.channel = 'ch0'
+mqtt_ch0_set.newvalue = 0.004
 
 
 # https://stackoverflow.com/questions/10252010/serializing-class-instance-to-json
@@ -71,20 +71,17 @@ mqtt_raw_01.rssi = -83
 
 
 while True:
-    random.seed()
-
-    mqtt_raw_01.rssi = random.randint(-39, -30)
 
     topic = 'api/logger/pollingfreq'
-    payload = json.dumps(mqtt_raw_01.__dict__)
+    payload = json.dumps(mqtt_ch0_set.__dict__)
     # print(payload)
     client.publish(topic=topic, payload=payload, qos=0, retain=False)
 
-    topic = 'api/skskd'
-    payload = json.dumps(mqtt_raw_01.__dict__)
+    topic = 'api//ch0/set'
+    payload = json.dumps(mqtt_ch0_set.__dict__)
     print(payload)
     client.publish(topic=topic, payload=payload, qos=0, retain=False)
 
 
 
-    time.sleep(4.0)
+    time.sleep(3.0)
